460cda2764265eae6d06ed9ababbb39fa46f5e40
Dim X <flamengist@gmail.com>
2015-04-03 19:59:11 +0300

    added Wextra flag to ycm_extra_conf

--- .ycm_extra_conf.py
+++ .ycm_extra_conf.py
@@ -38,6 +38,7 @@ flags = [
 #'-fPIC', # for shared libraries, might not be needed if using -fPIE
 '-fPIE', # for executables
 '-Wall',
+'-Wextra',
 '-DUSE_CLANG_COMPLETER',
 # THIS IS IMPORTANT! Without a "-std=<something>" flag, clang won't know which
 # language to use when compiling headers. So it will guess. Badly. So C++
